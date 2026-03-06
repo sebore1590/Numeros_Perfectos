7b5c4acf1c77c5b2df3686a1c944580266d85f3f
Sebastián Botero <sebore1590@gmail.com>
2026-03-06 17:09:21 -0500

    Fix(numeros_perfectos): ajuste
    Se agregan funciones para mejorar escalabilidad

--- numeros_perfectos.py
+++ numeros_perfectos.py
@@ -1,38 +1,51 @@
-
 import json
 
-limite_inicial = int(input("Ingrese desde que numero desea buscar números perfectos: "))
-limite_final = int(input("Ingrese hasta que limite desea buscar números perfectos: "))
-if limite_inicial > limite_final:
-    print(f"El limite inicial debe ser menor al limite final")
-    exit()
-if limite_inicial < 0:
-    print(f"El rango solo permite numeros positivos")
-    exit()
+def pedirNumeros():
+    limiteInicial = int(input("Ingrese desde que numero desea buscar números perfectos: "))
+    limiteFinal = int(input("Ingrese hasta que limite desea buscar números perfectos: "))
 
-print(f"Buscando números perfectos desde {limite_inicial} hasta {limite_final}...")
+    if limiteInicial > limiteFinal:
+        print("El limite inicial debe ser menor al limite final")
+        exit()
 
-numeros_perfectos = []
+    if limiteInicial < 0:
+        print("El rango solo permite numeros positivos")
+        exit()
 
-for num in range(limite_inicial, limite_final + 1):
-    suma_divisores = 0
+    return limiteInicial, limiteFinal
 
-    for i in range(1, (num // 2) + 1):
-        if num % i == 0:
-            suma_divisores += i
-    if suma_divisores == num:
+def calculaNumerosPerfectos(limiteInicial, limiteFinal):
+
+    numerosPerfectos = []
+
+    for num in range(limiteInicial, limiteFinal + 1):
+
+        sumaDivisores = 0
+
+        for i in range(1, (num // 2) + 1):
+            if num % i == 0:
+                sumaDivisores += i
+
+        if sumaDivisores == num:
             print(f"Numero perfecto encontrado: {num}")
-            numeros_perfectos.append(num)
+            numerosPerfectos.append(num)
+
+    return numerosPerfectos
+
+limiteInicial, limiteFinal = pedirNumeros()
+
+print(f"Buscando numeros perfectos desde {limiteInicial} hasta {limiteFinal}")
+
+numerosPerfectos = calculaNumerosPerfectos(limiteInicial, limiteFinal)
+
+print(f"Los numeros perfectos son: {numerosPerfectos}")
 
 datos = {
-    "limite_busqueda": f"{limite_inicial} - {limite_final}",
-    "numeros_perfectos": numeros_perfectos
+    "limite_busqueda": f"{limiteInicial} - {limiteFinal}",
+    "numeros_perfectos": numerosPerfectos
 }
 
-with open("numeros_perfectos.json", "w") as archivo: #crear
-    json.dump(datos, archivo, indent=4) #sobreescribir
+with open("numeros_perfectos.json", "w") as archivo:
+    json.dump(datos, archivo, indent=4)
 
 print("Resultados guardados en numeros_perfectos.json")
-
-
-
